540b47e4f5640fd0e6187be7c50cd56f1ce24a66
LooseSeal2 <lucile2@protonmail.com>
2019-07-18 14:31:22 -0700

    Standardized the quit/deploy, use Z instead of Q

--- pgshield.sh
+++ pgshield.sh
@@ -200,11 +200,11 @@ phase31() {
 
 $notrun
 
-💬 Quitting? TYPE > q or exit
+💬 To quit, type >>> z or exit
 EOF
   read -p '🌍 Type APP to disable PGShield | Press [ENTER]: ' typed </dev/tty
 
-  if [[ "$typed" == "exit" || "$typed" == "EXIT" || "$typed" == "EXIT" || "$typed" == "q" || "$typed" == "Q" ]]; then appexempt; fi
+  if [[ "$typed" == "exit" || "$typed" == "Exit" || "$typed" == "EXIT" || "$typed" == "z" || "$typed" == "Z" ]]; then appexempt; fi
 
   grep -w "$typed" /var/plexguide/program.temp >/var/plexguide/check55.sh
   usercheck=$(cat /var/plexguide/check55.sh)
@@ -257,11 +257,11 @@ phase21() {
 
 $notrun
 
-💬 Quitting? TYPE > q or exit
+💬 To quit, type >>> z or exit
 EOF
   read -p '🌍 Type app to enable PGShield | Press [ENTER]: ' typed </dev/tty
 
-  if [[ "$typed" == "exit" || "$typed" == "EXIT" || "$typed" == "EXIT" || "$typed" == "q" || "$typed" == "Q" ]]; then appexempt; fi
+  if [[ "$typed" == "exit" || "$typed" == "Exit" || "$typed" == "EXIT" || "$typed" == "z" || "$typed" == "Z" ]]; then appexempt; fi
 
   grep -w "$typed" /var/plexguide/pgshield.ex15 >/var/plexguide/check55.sh
   usercheck=$(cat /var/plexguide/check55.sh)
@@ -285,7 +285,7 @@ webid() {
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 🔑 Google Web Keys - Client ID       📓 Reference: pgshield.pgblitz.com
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-Quitting? TYPE > q or exit
+To quit, type >>> z or exit
 NOTE: Visit reference for Google Web Auth Keys
 
 EOF
